36438aaef4d1d3eef60190663c7d218e0c8c81c6
Stephan Seliuk <stepan@selyuk.com>
2019-08-10 16:19:51 +0300

    Cosmetic fixes;

--- build/run-nginx.sh
+++ build/run-nginx.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-trapIt () { "$@"& pid="$!"; for SGNL in INT TERM CHLD USR1; do trap "kill -$SGNL $pid" "$SGNL"; done; while kill -0 $pid > /dev/null 2>&1; do wait $pid; ec="$?"; done; exit $ec; };
+trapIt () { "$@"& pid="$!"; for SGNL in INT TERM CHLD USR1; do trap "kill -${SGNL} ${pid}" "$SGNL"; done; while kill -0 ${pid} > /dev/null 2>&1; do wait ${pid}; ec="$?"; done; exit ${ec}; };
 
 STATIC=/var/www/
 args=" $@ "
-if [[ ! $args =~ " --help " ]]; then
+if [[ ! ${args} =~ " --help " ]]; then
   ARG_STATIC=`/ppm/vendor/bin/ppm config --show-option="static-directory" "$@"`
 fi
 
-[ ! -z "$ARG_STATIC" ] && STATIC="${STATIC}${ARG_STATIC}"
+[[ ! -z "$ARG_STATIC" ]] && STATIC="${STATIC}${ARG_STATIC}"
 sed -i "s#STATIC_DIRECTORY#${STATIC}#g" /etc/nginx/sites-enabled/default
 
 nginx
@@ -20,4 +20,4 @@ ARGS='--port=8081 --socket-path=/ppm/run --pidfile=/ppm/ppm.pid'
 # make sure static-directory is not served by php-pm
 ARGS="$ARGS --static-directory=''"
 
-trapIt /ppm/vendor/bin/ppm start --ansi $ARGS $@
+trapIt /ppm/vendor/bin/ppm start --ansi ${ARGS} "$@"
