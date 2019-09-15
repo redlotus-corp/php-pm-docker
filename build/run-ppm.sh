73c1395624a2e5493b0fc5d9ad5cb48f9e2947cf
Stephan Seliuk <stepan@selyuk.com>
2019-09-15 17:42:47 +0300

    Added debugging stuff to trapIt function to remain scripts;

--- build/run-ppm.sh
+++ build/run-ppm.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-trapIt () { "$@"& pid="$!"; for SGNL in INT TERM CHLD USR1; do trap "kill -${SGNL} ${pid}" "$SGNL"; done; while kill -0 ${pid} > /dev/null 2>&1; do wait ${pid}; ec="$?"; done; exit ${ec}; };
+trapIt () {
+    "$@"& pid="$!";
+    echo "CMD: $@"
+    echo "PID: $pid"
+    for SGNL in INT TERM CHLD USR1; do
+        trap "ps ax; echo 'Trapped ${SGNL}. Sending signal ${SGNL} to PID ${pid}'; kill -${SGNL} ${pid} || true" "$SGNL";
+    done;
+    echo "Checking if pid ${pid} is still running"
+    while kill -0 ${pid} > /dev/null 2>&1; do
+        echo "Waiting ${pid} to complete..."
+        wait ${pid}; ec="$?";
+    done;
+    exit ${ec};
+};
 
 ARGS='--port 8080'
 trapIt /ppm/vendor/bin/ppm start --ansi "$@"
